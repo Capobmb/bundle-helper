c4ef408b1ec3e0f436d12b049407e42fbbc1330d
Capobmb <taip0321.h@gmail.com>
2024-11-23 23:57:59 +0900

    feat: support [un]comment {line,block} but with no command overwrite

--- cmd/main.go
+++ cmd/main.go
@@ -4,36 +4,170 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"log/slog"
 	"os"
+	"strings"
 )
 
 type LineConverter struct {
 }
 
-func (li LineConverter) ConvertLine(line string, parseState *ParseState) (string, error) {
-	// TODO: 実装
+type Line struct {
+	content    string
+	lineNumber int
+}
+
+func (li Line) String() string {
+	return fmt.Sprintf("line %d: %s", li.lineNumber, li.content)
+}
+
+func (li Line) SetContent(content string) Line {
+	li.content = content
+	return li
+}
+
+func (li LineConverter) ConvertLine(line Line, parseState *ParseState) (Line, error) {
+	const COMMENT_PREFIX = "//"
+	makeCommentLine := func(line string) string {
+		return COMMENT_PREFIX + line
+	}
+	switch parseState.Current {
+	case undefined:
+		return line, fmt.Errorf("undefined parse state detected: %v", parseState)
+	case identity:
+		return line, nil
+	case isSingleLineCommenting:
+		return line.SetContent(makeCommentLine(line.content)), nil
+	case isSingleLineUncommenting:
+		return line.SetContent(strings.TrimPrefix(line.content, COMMENT_PREFIX)), nil
+	case isBlockCommenting:
+		return line.SetContent(makeCommentLine(line.content)), nil
+	case isBlockUncommenting:
+		return line.SetContent(strings.TrimPrefix(line.content, COMMENT_PREFIX)), nil
+	}
 	return line, nil
 }
 
 type ParseState struct {
-	// TODO: Order を処理している途中かどうかを判定するフィールドの追加
+	Current AtomicParseState
+	Next    AtomicParseState
 }
 
-type Order string
+type AtomicParseState int
+
+const (
+	undefined = iota
+	identity
+	isSingleLineCommenting
+	isSingleLineUncommenting
+	isBlockCommenting
+	isBlockUncommenting
+	// TODO: add isErasingLine, isProducingCommentingCommand, isProducingUncommentingCommand
+)
+
+type CommandType string
+
+const (
+	CommentSingleLine   CommandType = "comment_single_line"
+	UncommentSingleLine CommandType = "uncomment_single_line"
+	CommentBlockBegin   CommandType = "comment_block_begin"
+	CommentBlockEnd     CommandType = "comment_block_end"
+	UncommentBlockBegin CommandType = "uncomment_block_begin"
+	UncommentBlockEnd   CommandType = "uncomment_block_end"
+)
+
+type CommandHandler func(state *ParseState) error
+
+type CommandProcessor struct {
+	handlers map[CommandType]CommandHandler
+}
+
+func NewCommandProcessor() *CommandProcessor {
+	return &CommandProcessor{
+		handlers: make(map[CommandType]CommandHandler),
+	}
+}
+
+func (p *CommandProcessor) Register(cmdType CommandType, handler CommandHandler) {
+	p.handlers[cmdType] = handler
+}
+
 type Converter struct {
-	lineConverter *LineConverter
-	parseState    *ParseState
+	lineConverter    *LineConverter
+	parseState       *ParseState
+	commandProcessor *CommandProcessor
 }
 
-func (ip Converter) ReadOrder(line string) Order {
-	// TODO: なんかsplit とかで実装
-	return ""
+func (c Converter) ReadOrder(line Line, cmdProcessor *CommandProcessor) error {
+	trimmedLine := strings.Trim(line.content, " ")
+	const COMMENTLINEPREFIX = "//"
+	if !strings.HasPrefix(trimmedLine, COMMENTLINEPREFIX) {
+		// not an comment, returning
+		slog.Debug("not a comment line", "line", line)
+		return nil
+	} else {
+		slog.Debug("comment line", "line", line)
+	}
+	trimmedLine = strings.TrimPrefix(trimmedLine, COMMENTLINEPREFIX)
+	trimmedLine = strings.Trim(trimmedLine, " ")
+
+	slog.Debug("trimmedLine", "trimmedLine", trimmedLine)
+	splittedLine := strings.Split(trimmedLine, " ")
+	for i := range splittedLine {
+		splittedLine[i] = strings.Trim(splittedLine[i], " ")
+	}
+	// debug print splittedLine
+	slog.Debug("splittedLine", "splittedLine", splittedLine, "size", len(splittedLine))
+
+	const COMMANDPREFIX = "@bundle-helper"
+	if len(splittedLine) < 1 || splittedLine[0] != COMMANDPREFIX {
+		// not a command, returning
+		return nil
+	}
+	// this line is a command
+
+	if len(splittedLine) < 2 {
+		return fmt.Errorf("command argument not enough. line content: %s", line)
+	}
+	commandTypeStr := splittedLine[1]
+	var commandType CommandType
+	for cmdType := range cmdProcessor.handlers {
+		if string(cmdType) == commandTypeStr {
+			commandType = cmdType
+		}
+	}
+	if commandType == "" {
+		return fmt.Errorf("unknown command type `%s` in line `%s`", commandType, line)
+	}
+	slog.Debug("command type", "commandType", commandType)
+
+	handler, found := cmdProcessor.handlers[commandType]
+	if !found {
+		return fmt.Errorf("unknown command type `%s` in line `%s`", commandType, line)
+	}
+
+	// handle
+	err := handler(c.parseState)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
-func (ip Converter) ExecOrder(line string, order Order, parseState *ParseState) {
-	// TODO: parseState をoverwrite
-	// TODO: そういえばline も書き換えないといけねえなあ
+
+func (lc LineConverter) NotifyConvertLineFinish(parseState *ParseState) error {
+	parseState.Current = parseState.Next
+	parseState.Next = identity
+	if parseState.Current == isBlockCommenting {
+		parseState.Next = isBlockCommenting
+	}
+	if parseState.Current == isBlockUncommenting {
+		parseState.Next = isBlockUncommenting
+	}
+	return nil
 }
-func (ip Converter) Convert(fileName string) ([]string, error) {
+
+func (c Converter) Convert(fileName string) ([]Line, error) {
 	file, err := os.Open(fileName)
 	if err != nil {
 		return nil, fmt.Errorf("can not open file: %w", err)
@@ -41,28 +175,29 @@ func (ip Converter) Convert(fileName string) ([]string, error) {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	var resultLines []string
+	var resultLines []Line
 
-	for scanner.Scan() {
-		line := scanner.Text()
-		order := ip.ReadOrder(line)
-		if order != "" {
-			// order あります！
-			ip.ExecOrder(line, order, ip.parseState)
-			resultLines = append(resultLines, line)
-			continue
+	for i := 0; scanner.Scan(); i++ {
+		lineContent := scanner.Text()
+		currentLine := Line{lineContent, i}
+		slog.Debug("line", "lineNumber", i, "current parse state", c.parseState)
+		err := c.ReadOrder(currentLine, c.commandProcessor)
+		if err != nil {
+			return nil, fmt.Errorf("command read error: %w", err)
 		}
-		convertedLine, err := ip.lineConverter.ConvertLine(line, ip.parseState)
+		slog.Debug("line", "lineNumber", i, "after read order", "current parse state", c.parseState)
+		convertedLine, err := c.lineConverter.ConvertLine(currentLine, c.parseState)
 		if err != nil {
 			return nil, fmt.Errorf("変換失敗！: %w", err)
 		}
 		resultLines = append(resultLines, convertedLine)
+		c.lineConverter.NotifyConvertLineFinish(c.parseState)
 	}
 
 	return resultLines, nil
 }
 
-func writeLines(fileName string, lines []string) error {
+func writeLines(fileName string, lines []Line) error {
 	file, err := os.Create(fileName)
 	if err != nil {
 		return fmt.Errorf("cannot open file %s: %w", fileName, err)
@@ -71,7 +206,7 @@ func writeLines(fileName string, lines []string) error {
 
 	writer := bufio.NewWriter(file)
 	for _, line := range lines {
-		_, err = fmt.Fprintln(writer, line)
+		_, err = fmt.Fprintln(writer, line.content)
 		if err != nil {
 			return fmt.Errorf("cannot write to file %s: %w", fileName, err)
 		}
@@ -85,9 +220,58 @@ func writeLines(fileName string, lines []string) error {
 }
 
 func main() {
-	var lineConverter LineConverter
-	var parseState ParseState
-	converter := Converter{&lineConverter, &parseState}
+	// change default logger to debug level
+	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelDebug})))
+
+	lineConverter := LineConverter{}
+	parseState := ParseState{identity, identity}
+	commandProcessor := NewCommandProcessor()
+
+	// register handlers
+	commandProcessor.Register(CommentSingleLine, func(state *ParseState) error {
+		state.Current = identity
+		state.Next = isSingleLineCommenting
+		return nil
+	})
+	commandProcessor.Register(UncommentSingleLine, func(state *ParseState) error {
+		state.Current = identity
+		state.Next = isSingleLineUncommenting
+		return nil
+	})
+	commandProcessor.Register(CommentBlockBegin, func(state *ParseState) error {
+		if state.Current == isBlockCommenting {
+			return fmt.Errorf("nested block commenting is not implemented for now")
+		}
+		state.Current = identity
+		state.Next = isBlockCommenting
+		return nil
+	})
+	commandProcessor.Register(UncommentBlockBegin, func(state *ParseState) error {
+		if state.Current == isBlockUncommenting {
+			return fmt.Errorf("nested block uncommenting is not implemented for now")
+		}
+		state.Current = identity
+		state.Next = isBlockUncommenting
+		return nil
+	})
+	commandProcessor.Register(CommentBlockEnd, func(state *ParseState) error {
+		if state.Current != isBlockCommenting {
+			return fmt.Errorf("unpaired comment block end detected")
+		}
+		state.Current = identity
+		state.Next = identity
+		return nil
+	})
+	commandProcessor.Register(UncommentBlockEnd, func(state *ParseState) error {
+		if state.Current != isBlockUncommenting {
+			return fmt.Errorf("unpaired uncomment block end detected")
+		}
+		state.Current = identity
+		state.Next = identity
+		return nil
+	})
+
+	converter := Converter{&lineConverter, &parseState, commandProcessor}
 
 	fileName := os.Args[1]
 	lines, err := converter.Convert(fileName)
@@ -95,7 +279,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	outFileName := fileName + ".converted"
+	outFileName := fileName + ".converted.cpp"
 	err = writeLines(outFileName, lines)
 	if err != nil {
 		log.Fatal(err)
